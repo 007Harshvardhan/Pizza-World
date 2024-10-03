86302e490d509a1d3a397488b03e32cc7aa76b88
Harsh Vardhan <harshvardhan1412002@gmail.com>
2024-10-04 05:23:07 +0530

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Home from './ui/Home';
-import Error from './ui/Error';
-import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import CreateOrder, {
   action as createOrderAction,
 } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import { action as updateOrderAction } from './features/order/UpdateOrder';
+import Error from './ui/Error';
+import Home from './ui/Home';
 
 import AppLayout from './ui/AppLayout';
 
--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,7 +1,6 @@
 export async function getAddress({ latitude, longitude }) {
   const res = await fetch(
-    `https://apis.mapmyindia.com/advancedmaps/v1/YOUR_API_KEY/reverse_geocode?lat=28.613939&lng=77.209021&format=json
-`
+    `https://apis.mapmyindia.com/advancedmaps/v1/YOUR_API_KEY/reverse_geocode?lat=28.613939&lng=77.209021&format=json`
   );
   if (!res.ok) throw Error("Failed getting address");
 
